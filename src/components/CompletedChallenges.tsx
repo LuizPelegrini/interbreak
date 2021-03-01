26d5f38b7c82d3036fc471e630959c14c36197ed
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2021-03-01 23:50:10 +0800

    Display challenges completed using the Challenge context

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,16 @@
 import styles from '../styles/components/CompletedChallenges.module.css';
 
+import { useContext } from 'react';
+import { ChallengeContext } from '../contexts/ChallengeContext';
+
 export default function CompletedChallenges(){
+  // get data from context to show the number of challenges completed
+  const { challengesCompleted } = useContext(ChallengeContext);
+
   return (
     <div className={styles.completedChallengesContainer}>
       <span>Challenges Completed</span>
-      <span>5</span>
+      <span>{challengesCompleted}</span>
     </div>
   );
 }
